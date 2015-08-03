e788c0cf2991c2b6f0772779a6b567846e2d3212
MohamedBassem <medoox240@gmail.com>
2015-08-03 09:48:33 +0200

    Adding YCM to vim installation script

--- vim-setup.sh
+++ vim-setup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-sudo apt-get install -y vim git
+sudo apt-get install -y vim git cmake python-dev
 pushd ~/
 wget https://raw.githubusercontent.com/MohamedBassem/dotfiles/master/.vimrc
 popd
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
+
+# For YCM
+cd ~/.vim/bundle/YouCompleteMe
+./install.sh --clang-completer
 
