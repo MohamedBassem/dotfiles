099c7122306b72544cc22da6fabe1881a00c3218
MohamedBassem <medoox240@gmail.com>
2015-11-30 10:38:01 +0200

    Some customization for i3

---
+++ .i3/home_two_screens.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI1 --off --output VIRTUAL1 --off --output eDP1 --primary --mode 1366x768 --pos 1600x132 --rotate normal --output VGA1 --mode 1600x900 --pos 0x0 --rotate normal
