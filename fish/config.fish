2cbf4b2722b1dc6e4f2a03e4dda68bab4780faf3
MohamedBassem <me@mbassem.com>
2024-01-17 01:56:38 +0000

    [shell] Basic fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,8 @@
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    atuin init fish --disable-up-arrow | source
 end
+
+abbr -a vim nvim
+abbr -a vi nvim
