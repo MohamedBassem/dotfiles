0f72bd16b1d6f740d73332058f9e6d1ead4fea80
MohamedBassem <me@mbassem.com>
2024-01-17 02:27:26 +0000

    Update the installation script

--- install.sh
+++ install.sh
@@ -4,10 +4,8 @@
 brew install git htop tmux curl
 
 #Cloning dotfiles
-ls -sf "`pwd`/.bashrc" ~/.bashrc
+ls -sf "`pwd`/fish" ~/.config/fish
+ls -sf "`pwd`/nvim" ~/.config/nvim
 ls -sf "`pwd`/.gitignore" ~/.gitignore
 ls -sf "`pwd`/.gitconfig" ~/.gitconfig
 ls -sf "`pwd`/.tmux.conf" ~/.tmux.conf
-
-# Install vim
-./vim-setup.sh
