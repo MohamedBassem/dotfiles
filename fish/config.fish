739e4825a82cb784c913d398f47dfd566b953808
MohamedBassem <me@mbassem.com>
2024-05-19 17:35:26 +0100

    [fish] Add volta

--- fish/config.fish
+++ fish/config.fish
@@ -13,3 +13,5 @@ set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
 
 set --export ANDROID_HOME "$HOME/Library/Android/sdk"
+set -gx VOLTA_HOME "$HOME/.volta"
+set -gx PATH "$VOLTA_HOME/bin" $PATH
