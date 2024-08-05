a6cea8594507bcd326eca7ea89f4366e1e984757
MohamedBassem <me@mbassem.com>
2024-08-05 02:59:02 +0000

    [fish] Add support for brew for linux

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,11 @@
-eval "$(/opt/homebrew/bin/brew shellenv)"
+if test -f "/opt/homebrew/bin/brew"
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+end
+
+if test -f "/home/linuxbrew/.linuxbrew/bin/brew"
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+end
+
 direnv hook fish | source
 
 if status is-interactive
