ed8d78c646722281cf939d5c2e61354c618bbddf
Mohamed Bassem <medoox240@gmail.com>
2024-08-05 04:58:38 -0700

    [nvim] Remove treesitter from illuminate on large files

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -529,7 +529,13 @@ return {
 	{
 		"RRethy/vim-illuminate",
 		config = function()
-			require("illuminate").configure({})
+			require("illuminate").configure({
+				delay = 200,
+				large_file_cutoff = 2000,
+				large_file_overrides = {
+					providers = { "lsp" },
+				},
+			})
 		end,
 	},
 	{
