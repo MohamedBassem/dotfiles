9acbb6152f58014aa3b3587a4089a7cd28591239
MohamedBassem <me@mbassem.com>
2024-08-14 14:13:13 +0300

    [wezterm] Add simple wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,36 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- Color Scheme
+config.color_scheme = "Catppuccin Frappe"
+
+-- Font
+config.font = wezterm.font("MesloLGL Nerd Font")
+config.font_size = 14.0
+
+-- Disable tab bar (assuming the use of aerospace)
+config.enable_tab_bar = false
+
+-- Hide window decorations (only useful when using aerospace)
+config.window_decorations = "RESIZE"
+
+-- Increase scrollback lines
+config.scrollback_lines = 1000000
+config.enable_scroll_bar = true
+
+-- Prompt before closing window
+config.window_close_confirmation = "AlwaysPrompt"
+
+config.keys = {
+  -- Disable creating new tabs
+  {
+    key = "t",
+    mods = "SUPER",
+    action = wezterm.action.DisableDefaultAssignment,
+  },
+}
+
+return config
