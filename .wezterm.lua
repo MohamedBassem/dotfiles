e6aff02d2f631cf64d122ab2a56a3df07818f467
Mohamed Bassem <medoox240@gmail.com>
2024-08-29 07:24:39 -0700

    [wezterm] To bindings for alt+arrow to jump words

--- .wezterm.lua
+++ .wezterm.lua
@@ -31,6 +31,11 @@ config.keys = {
     mods = "SUPER",
     action = wezterm.action.DisableDefaultAssignment,
   },
+
+  -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+  {key="LeftArrow", mods="OPT", action=wezterm.action{SendString="\x1bb"}},
+  -- Make Option-Right equivalent to Alt-f; forward-word
+  {key="RightArrow", mods="OPT", action=wezterm.action{SendString="\x1bf"}},
 }
 
 return config
