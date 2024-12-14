dafad2475fa1342932196ef417e840385dcf0569
MohamedBassem <me@mbassem.com>
2024-12-14 23:46:36 +0000

    [nvim] Add lib path to neodev

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -512,7 +512,14 @@ return {
 	{
 		"folke/lazydev.nvim",
 		ft = "lua",
-		opts = {},
+		opts = {
+			library = {
+				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+				{ path = "LazyVim", words = { "LazyVim" } },
+				{ path = "snacks.nvim", words = { "Snacks" } },
+				{ path = "lazy.nvim", words = { "LazyVim" } },
+			},
+		},
 	},
 	{
 		'MeanderingProgrammer/markdown.nvim',
