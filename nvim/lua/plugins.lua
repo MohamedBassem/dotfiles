47b3a9055dfe30eb12a1dea092e10ed0df853a17
MohamedBassem <me@mbassem.com>
2024-12-14 22:44:22 +0000

    [nvim] Enable smooth scroll

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -716,6 +716,7 @@ return {
             },
 			-- When doing nvim somefile.txt, it will render the file as quickly as possible, before loading your plugins.
 			quickfile = { enabled = true },
+			scroll = { enabled = true },
 			gitbrowse = { enabled = true },
 			statuscolumn = { enabled = true },
 			styles = {
