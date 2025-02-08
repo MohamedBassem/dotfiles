0652ba0124d4eec58de4cb8facb9af2d5c681c16
MohamedBassem <me@mbassem.com>
2025-02-08 20:40:56 +0000

    [nvim] Upgrade a bunch of plugins

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -597,7 +597,7 @@ return {
 	},
 	{
 		"pmizio/typescript-tools.nvim",
-		dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig", "VonHeikemen/lsp-zero.nvim" },
+		dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
 		opts = {
 			on_attach = function(client)
 				-- Formatting is handled by none-ls
