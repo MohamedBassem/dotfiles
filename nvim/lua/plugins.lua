cecccc24718579f1f8ee5a5a5762d4bda94e4899
MohamedBassem <me@mbassem.com>
2025-03-30 02:54:03 +0000

    [nvim] Upgrade blink to 1.0

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -215,7 +215,10 @@ return {
 			-- elsewhere in your config, without redefining it, due to `opts_extend`
 			sources = {
 				default = { 'lsp', 'path', 'snippets', 'buffer' },
-				cmdline = {},
+			},
+			cmdline = {
+				enabled = false,
+				sources = {},
 			},
 			signature = {
 				enabled = true,
