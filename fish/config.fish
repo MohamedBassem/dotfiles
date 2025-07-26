8bba3c57b57bbe6dd15fee6e8816319fd635069c
MohamedBassem <me@mbassem.com>
2025-07-26 19:57:03 +0100

    fish: Add more stuff

--- fish/config.fish
+++ fish/config.fish
@@ -22,3 +22,12 @@ set --export PATH $BUN_INSTALL/bin $PATH
 set --export ANDROID_HOME "$HOME/Library/Android/sdk"
 set -gx VOLTA_HOME "$HOME/.volta"
 set -gx PATH "$VOLTA_HOME/bin" $PATH
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
+
+# Added by LM Studio CLI (lms)
+set -gx PATH $PATH /Users/mohamedbassem/.lmstudio/bin
+
+alias claude="/Users/mohamedbassem/.claude/local/claude"
