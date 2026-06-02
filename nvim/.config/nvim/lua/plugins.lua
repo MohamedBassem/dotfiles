8a231864c9de98d4acf3c8e9827077b3167f6de8
MohamedBassem <me@mbassem.com>
2026-06-02 06:59:51 +0100

    theme: we're doing catppuccin macchiato now

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -5,7 +5,7 @@ return {
 		priority = 1000,
 		config = function()
 			require("catppuccin").setup({
-				flavour = "frappe",
+				flavour = "macchiato",
 				integrations = {
 					fidget = true,
 					mason = true,
